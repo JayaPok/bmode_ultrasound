9e596b9fb5876e77e303a5a8ff443a139f4e6583
Aniruddh Prakash <Aniruddh Prakash>
2016-11-13 18:48:41 -0500

    updated test file and removed typo #15

--- test_read_JSON.py
+++ test_read_JSON.py
@@ -5,6 +5,6 @@ def test_read_JSON_data():
     c, fs, axial_samples, beam_spacing, num_beams = read_JSON_data('bmode.JSON')
     assert c == 1540
     assert fs == 40000000
-    assert axial_samples == 156
+    assert axial_samples == 1556
     assert beam_spacing == 0.00011746274509803921
     assert num_beams == 256
