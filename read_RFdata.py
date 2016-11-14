9b6c21e7aeb4df70a4a8ac4e90ae8c5d4956cb6b
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-11-14 03:11:13 -0500

    created functions for reading and splitting RF data

---
+++ read_RFdata.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def read_RF(filename):
+	rf_data = np.array(np.fromfile(filename, dtype = 'uint16', count = -1, sep=""))
+
+	return rf_data
+
+def RF_bars(rf_data, num_beams):
+    rfdata_bars = np.split(rf_data, num_beams)
+
+    return rfdata_bars
