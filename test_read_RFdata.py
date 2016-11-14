65d9b8872a707af51ce15acbeebbe51bf6d71e66
Aniruddh Prakash <Aniruddh Prakash>
2016-11-14 11:12:36 -0500

    consolidation of two test files into one file and PEP8 violations

--- test_read_RFdata.py
+++ test_read_RFdata.py
@@ -1,8 +1,14 @@
-from read_RFdata import read_RF
+from read_RFdata import read_RF, RF_bars
+import numpy as np
+
 
 def test_read_RFdata():
-	import numpy as np
-	
-	bin_data = read_RF('test.bin')
+    bin_data = read_RF('test.bin')
+    assert bin_data == np.array([770])
 
-	assert bin_data == np.array([770])
+
+def test_RF_bars():
+
+    sample_points = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+    sample_split = RF_bars(sample_points, 3)
+    assert np.any(sample_split) == np.any([np.array([1, 2, 3, 4, 5]), np.array([6, 7, 8, 9, 10]), np.array([11, 12, 13, 14, 15])])
