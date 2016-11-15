3edb2cb6c34c900d3d59f7929e4b158b363a6286
Aniruddh Prakash <Aniruddh Prakash>
2016-11-14 19:19:57 -0500

    updated unit test file #19

--- test_B_mode_image_creation.py
+++ test_B_mode_image_creation.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def test_envelope_detection():
-    data_a = [np.array([1, -2, 3, 4, 5]), np.array([6, 7, -8, 9, -10]), np.array([11, -12, 13, 14, -15])]
+    data_a = [np.array([1, -2, 3, -4, 5]), np.array([6, -7, -8, 9, -10]), np.array([11, -12, 13, -14, -15])]
     a = envelope_detection(data_a, 3)
     assert np.any(a) == np.any([np.array([1.5, 2, 3, 4, 4.5]), np.array([6.5, 7, 8, 9, 9.5]),
                                 np.array([11.5, 12, 13, 14, 14.5])])
