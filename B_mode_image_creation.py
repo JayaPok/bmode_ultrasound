6f1cabf8e771d559abc0e1258d555c5f1003adbd
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-11-17 17:04:22 -0500

    implemented logarithic compression to data

--- B_mode_image_creation.py
+++ B_mode_image_creation.py
@@ -11,3 +11,9 @@ def array_filtering(RF_array):
         filtered = lowess(RF_array_T[:, i], x_data, frac=0.05)
         RF_array_filtered[:, i] = filtered[:, 1]
     return RF_array_filtered
+
+def logarithmic_compression(RF_array_filtered):
+    log_RFarray_filtered = np.empty([len(RF_array_filtered), len(RF_array_filtered[0])])
+    for i in range(len(RF_array_filtered[0])):
+        log_RFarray_filtered[:, i] = np.log10(RF_array_filtered[:, i])
+    return log_RFarray_filtered
