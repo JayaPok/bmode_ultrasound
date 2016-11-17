0300d7f880c1c38902e16ea4fd034d7ea2709280
Aniruddh Prakash <Aniruddh Prakash>
2016-11-17 15:58:14 -0500

    updated unit test file for saving file, #21

---
+++ test_B_mode_outputs.py
@@ -0,0 +1,18 @@
+from B_mode_outputs import image_save
+import numpy as np
+import os
+import pytest
+
+B_mode_array = np.absolute(np.empty([5, 5]))
+
+
+def test_image_save():
+    assert not os.path.isfile('bmode.png')
+    image_save('bmode.png', B_mode_array)
+    assert os.path.isfile('bmode.png')
+    os.remove('bmode.png')
+
+
+def test_image_save_wrong_type():
+    with pytest.raises(ValueError):
+        image_save('bmode.bmp', B_mode_array)
