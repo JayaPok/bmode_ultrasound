79c7808de6c69a2091732d25600e384a117ba0ac
Aniruddh Prakash <Aniruddh Prakash>
2016-11-18 03:10:41 -0500

    added unit test for axes for plotting using matplotlib, #22

--- test_B_mode_outputs.py
+++ test_B_mode_outputs.py
@@ -1,9 +1,17 @@
-from B_mode_outputs import image_save
+from B_mode_outputs import image_save, image_plot
 import numpy as np
 import os
 import pytest
+import matplotlib.pyplot as plt
 
 B_mode_array = np.absolute(np.empty([5, 5]))
+
+
+def test_image_plot():
+    output = image_plot(B_mode_array, 10, 100, 5, 100, 1000)
+    plt.close()
+    assert output == [0, 50, 500,0]
+
 
 
 def test_image_save():
