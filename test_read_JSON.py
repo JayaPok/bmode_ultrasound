0710c548460907ed78866775e12c5bbb657aab54
Aniruddh Prakash <Aniruddh Prakash>
2016-11-18 01:41:32 -0500

    update read JSON unit test, added unit tests for missing data, missing file, and wron data type

--- test_read_JSON.py
+++ test_read_JSON.py
@@ -1,4 +1,5 @@
 from read_JSON import read_JSON_data
+import pytest
 
 
 def test_read_JSON_data():
@@ -8,3 +9,18 @@ def test_read_JSON_data():
     assert axial_samples == 1556
     assert beam_spacing == 0.00011746274509803921
     assert num_beams == 256
+
+
+def test_read_JSON_data_missing_file():
+    with pytest.raises(FileNotFoundError):
+        read_JSON_data('banana.json')
+
+
+def test_read_JSON_data_missing_data():
+    with pytest.raises (TypeError):
+        read_JSON_data('bmode_missing_value.json')
+
+
+def test_read_JSON_data_wrong_data_type():
+    with pytest.raises (ValueError):
+        read_JSON_data('bmode_string_value.json')
