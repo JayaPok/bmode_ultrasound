bd757eb7534b4d8463c856e196a02451c0ab0977
Aniruddh Prakash <Aniruddh Prakash>
2016-11-18 23:25:41 -0500

    main file for B-mode project

---
+++ main_B_mode_file.py
@@ -0,0 +1,25 @@
+from read_JSON import read_JSON_data
+from read_RFdata import read_RF, RF_bars
+from B_mode_image_creation import array_filtering, logarithmic_compression
+from B_mode_outputs import image_save, image_plot
+from argparse_input import main_args
+import logging
+
+
+if __name__ == "__main__":
+
+    JSON_filename, RF_data_filename, image_filename, display, save = main_args()
+
+    logging.basicConfig(filename='Logging_file.log',
+                        filemode='w', level=logging.DEBUG,
+                        format='%(asctime)s %(message)s')
+
+    c, fs, axial_samples, beam_spacing, num_beams = read_JSON_data(JSON_filename)
+    rf_data = read_RF(RF_data_filename)
+    rf_data_bars = RF_bars(rf_data, num_beams)
+    rf_array_filtered = array_filtering(rf_data_bars)
+    log_rf_array_filtered = logarithmic_compression(rf_array_filtered)
+    if save:
+        image_save(image_filename, log_rf_array_filtered)
+    if display:
+        image_plot(log_rf_array_filtered, beam_spacing, axial_samples, num_beams, fs, c)
