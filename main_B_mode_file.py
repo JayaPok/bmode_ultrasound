00ff8b2abaf515c886156e9c9a4f734bfd98a9a8
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-11-20 00:53:38 -0500

    made changes to runner

--- main_B_mode_file.py
+++ main_B_mode_file.py
@@ -1,6 +1,6 @@
 from read_JSON import read_JSON_data
 from read_RFdata import read_RF, RF_bars
-from B_mode_image_creation import array_filtering, logarithmic_compression
+from B_mode_image_creation import array_filtering, logarithmic_compression, equalization
 from B_mode_outputs import image_save, image_plot
 from argparse_input import main_args
 import logging
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     rf_data_bars = RF_bars(rf_data, num_beams)
     rf_array_filtered = array_filtering(rf_data_bars)
     log_rf_array_filtered = logarithmic_compression(rf_array_filtered)
+    RFarray_equalized = equalization(log_rf_array_filtered)
     if save:
-        image_save(image_filename, log_rf_array_filtered)
+        image_save(image_filename, RFarray_equalized)
     if display:
-        image_plot(log_rf_array_filtered, beam_spacing, axial_samples, num_beams, fs, c)
+        image_plot(RFarray_equalized, beam_spacing, axial_samples, num_beams, fs, c)
