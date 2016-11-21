d7e372ca5f814cd065bf0d2f600692d80cc94ac3
Jaya Pokuri <jaya.pokuri@gmail.com>
2016-11-20 22:41:59 -0500

    fixed some PEP8 violations

--- B_mode_outputs.py
+++ B_mode_outputs.py
@@ -6,7 +6,8 @@ def image_plot(B_mode_array, beam_spacing, axial_samples, num_beams, f_s, c):
     """Calculate Lateral distance and depth of image in meters using data
     from JSON file and display file using matplotlib
 
-    :param B_mode_array: completed 2D numpy array of B-mode data that has undergone all image processing
+    :param B_mode_array: completed 2D numpy array of
+     B-mode data that has undergone all image processing
     :param beam_spacing: distance between beams in meters
     :param axial_samples: # of samples in the axial direction
     :param num_beams: number of beams
@@ -30,19 +31,21 @@ def image_plot(B_mode_array, beam_spacing, axial_samples, num_beams, f_s, c):
     plt.show()
 
 
-def image_save(image_filename, B_mode_array, beam_spacing, axial_samples, num_beams, f_s, c):
+def image_save(image_filename, B_mode_array, beam_spacing,
+               axial_samples, num_beams, f_s, c):
     """
 
     :param image_filename: User specified filename to save B-mode image to
-    :param B_mode_array: completed 2D numpy array of B-mode data that has undergone all image processing
+    :param B_mode_array: completed 2D numpy array of B-mode
+     data that has undergone all image processing
     :return: None
     """
     try:
         lateral_distance = beam_spacing * num_beams
         depth_distance = c * axial_samples / f_s / 2
         extent_array = [0, lateral_distance, depth_distance, 0]
-        plt.imshow(B_mode_array, aspect='auto', 
-          extent=extent_array, cmap='Greys_r')
+        plt.imshow(B_mode_array, aspect='auto',
+                   extent=extent_array, cmap='Greys_r')
         plt.title('B-mode Ultrasound Image')
         plt.xlabel('Depth (m)')
         plt.ylabel('Distance (m)')
--- main_B_mode_file.py
+++ main_B_mode_file.py
@@ -1,6 +1,7 @@
 from read_JSON import read_JSON_data
 from read_RFdata import read_RF, RF_bars
-from B_mode_image_creation import array_filtering, logarithmic_compression, equalization
+from B_mode_image_creation import array_filtering,\
+    logarithmic_compression, equalization
 from B_mode_outputs import image_save, image_plot
 from argparse_input import main_args
 import logging
@@ -8,19 +9,23 @@ import logging
 
 if __name__ == "__main__":
 
-    JSON_filename, RF_data_filename, image_filename, display, save = main_args()
+    JSON_filename, RF_data_filename, image_filename, display, save =\
+        main_args()
 
     logging.basicConfig(filename='Logging_file.log',
                         filemode='w', level=logging.DEBUG,
                         format='%(asctime)s %(message)s')
 
-    c, fs, axial_samples, beam_spacing, num_beams = read_JSON_data(JSON_filename)
+    c, fs, axial_samples, beam_spacing, num_beams = \
+        read_JSON_data(JSON_filename)
     rf_data = read_RF(RF_data_filename, axial_samples, num_beams)
     rf_data_bars = RF_bars(rf_data, num_beams)
     rf_array_filtered = array_filtering(rf_data_bars)
     log_rf_array_filtered = logarithmic_compression(rf_array_filtered)
     RFarray_equalized = equalization(log_rf_array_filtered)
     if save:
-        image_save(image_filename, RFarray_equalized, beam_spacing, axial_samples, num_beams, fs, c)
+        image_save(image_filename, RFarray_equalized, beam_spacing,
+                   axial_samples, num_beams, fs, c)
     if display:
-        image_plot(RFarray_equalized, beam_spacing, axial_samples, num_beams, fs, c)
+        image_plot(RFarray_equalized, beam_spacing, axial_samples,
+                   num_beams, fs, c)
